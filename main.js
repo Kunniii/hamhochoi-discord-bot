070c7afde74dd3b73a18b0f83cc0102cfaaa2819
Kunniii <nguyenairport56@gmail.com>
2023-04-09 01:00:18 +0700

    main.js

--- main.js
+++ main.js
@@ -3,6 +3,30 @@ import { TOKEN, LISTEN_TO, APPLICATION_ID } from "./utils/env.js";
 import { Client, Events, GatewayIntentBits, ActivityType, REST, Routes } from "discord.js";
 import { getCategory } from "./utils/index.js";
 
+//#region BEGIN REGISTER COMMAND
+import { commands } from "./commands/generals.js";
+const rest = new REST({ version: 10 }).setToken(TOKEN);
+
+const slashCommands = [];
+
+for (const name in commands) {
+  const command = commands[name];
+  slashCommands.push(command.data.toJSON());
+}
+
+(async () => {
+  try {
+    console.log(`Registering ${slashCommands.length} commands!`);
+    const data = await rest.put(Routes.applicationCommands(APPLICATION_ID), {
+      body: slashCommands,
+    });
+    console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+  } catch (err) {
+    console.log(err.message);
+  }
+})();
+//#endregion END REGISTER COMMAND
+
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 const handleInteraction = async (interaction) => {
@@ -33,26 +57,3 @@ client.once(Events.ClientReady, (c) => {
     status: "online",
   });
 });
-
-// --- Register commands
-import { commands } from "./commands/generals.js";
-const rest = new REST({ version: 10 }).setToken(TOKEN);
-
-const slashCommands = [];
-
-for (const name in commands) {
-  const command = commands[name];
-  slashCommands.push(command.data.toJSON());
-}
-
-(async () => {
-  try {
-    console.log(`Registering ${slashCommands.length} commands!`);
-    const data = await rest.put(Routes.applicationCommands(APPLICATION_ID), {
-      body: slashCommands,
-    });
-    console.log(`Successfully reloaded ${data.length} application (/) commands.`);
-  } catch (err) {
-    console.log(err.message);
-  }
-})();
