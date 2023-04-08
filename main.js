e0e8e872f8605aa3d5f676db550b0556cb644ad0
Kunniii <nguyenairport56@gmail.com>
2023-04-09 00:53:10 +0700

    package.json

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import { commands as generalsCommands } from "./commands/generals.js";
-import { TOKEN, LISTEN_TO } from "./utils/env.js";
-import { Client, Events, GatewayIntentBits, ActivityType } from "discord.js";
+import { TOKEN, LISTEN_TO, APPLICATION_ID } from "./utils/env.js";
+import { Client, Events, GatewayIntentBits, ActivityType, REST, Routes } from "discord.js";
 import { getCategory } from "./utils/index.js";
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
@@ -33,3 +33,26 @@ client.once(Events.ClientReady, (c) => {
     status: "online",
   });
 });
+
+// --- Register commands
+import { commands } from "./commands/generals.js";
+const rest = new REST({ version: 10 }).setToken(TOKEN);
+
+const slashCommands = [];
+
+for (const name in commands) {
+  const command = commands[name];
+  slashCommands.push(command.data.toJSON());
+}
+
+(async () => {
+  try {
+    console.log(`Registering ${slashCommands.length} commands!`);
+    const data = await rest.put(Routes.applicationCommands(APPLICATION_ID), {
+      body: slashCommands,
+    });
+    console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+  } catch (err) {
+    console.log(err.message);
+  }
+})();
