7e0d3444bd3cc28b7ecd31e307738ab80726fe48
Kunniii <nguyenairport56@gmail.com>
2023-04-09 00:40:09 +0700

    check if the env is OK

--- utils/env.js
+++ utils/env.js
@@ -5,3 +5,5 @@ export const APPLICATION_ID = process.env.APPLICATION_ID;
 export const PUBLIC_KEY = process.env.PUBLIC_KEY;
 export const TOKEN = process.env.TOKEN;
 export const GUILD_ID = process.env.GUILD_ID;
+
+console.log(APPLICATION_ID);
